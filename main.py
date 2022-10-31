0a04b3d0c8b273d9f9e3cefa9abfbf93c46d79f9
Tinashe Madanire <madtinashe@gmail.com>
2022-11-01 00:24:26 +0200

    print password to string
    prints the password to one string

--- main.py
+++ main.py
@@ -1,23 +1,38 @@
+from colorama import Fore
 import random 
 
 chars = [
     ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 
 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'],
-['0','1', '2', '3', '4', '5', 
+['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 
+'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z','0','1', '2', '3', '4', '5', 
 '6', '7', '8', '9','@', '!', '#', '$', 
 '%', '^', '&', '*', '(', ')', '-', '_', '+']
 ]
 
 password = []
 
+pWord = ""
+
 def genPW():
     x = 1 
     password.append(chars[0][random.randint(0,51)])
     while x < 10:
-        password.append(chars[random.randint(0,1)][random.randint(0,22)])
+        password.append(chars[1][random.randint(0,73)])
         x += 1
-    
-genPW()
-print(password)
+
+def dispPW(lList, str):
+    for l in lList:
+        str = str + l
+
+    print(Fore.YELLOW, str)
+
+
+if __name__ == '__main__':
+    genPW()
+    dispPW(password, pWord)
+
+
 
